874648004f9dddd39ae3f9f74e3b0d8559ab907f
Ronin <bhabyaprakash01@gmail.com>
2025-06-07 11:36:28 +0530

    Update post.py

--- Scott/plugins/content/post.py
+++ Scott/plugins/content/post.py
@@ -2,6 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from Scott import app
 
-@app.on_message(filters.private & filters.command("post"))
+@app.on_message(filters.private & filters.command(["post"], prefixes=["/"]))
 async def handle_post(client, message: Message):
+    print("Post command received")
     await message.reply("hello I'm ready to post")
